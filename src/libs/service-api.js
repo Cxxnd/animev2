ef6b3b98386b657ecf6c63fe6cccfbb68e983ce6
Cxxnd <bwong0227@gmail.com>
2025-11-04 20:22:34 +0700

    service-api.js

--- src/libs/service-api.js
+++ src/libs/service-api.js
@@ -1,6 +1,6 @@
 export const getAnime = async ({ resource, query }) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`, {
-        next: { revalidate: 60 }
+        next: { revalidate: 100 }
     })
     const data = await response.json()
     return data
