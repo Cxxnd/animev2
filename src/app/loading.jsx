d33fe39e2d919828818bdd0f069f3a74cd126471
Cxxnd <bwong0227@gmail.com>
2025-11-20 01:46:40 +0700

    issue

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,7 @@
 import "../components/css/loading.css";
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center">
+        <div className=" h-screen flex items-center justify-center">
             <span className="loader"></span>
         </div>
     );
