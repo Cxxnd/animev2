c97d326e2ee04c77fde54fe9ca8b7e801ff264b7
Cxxnd <bwong0227@gmail.com>
2025-11-20 02:06:29 +0700

    modified episode

---
+++ src/app/episode/[slug]/loading.jsx
@@ -0,0 +1,17 @@
+"use client";
+import "../../../components/css/loading.css";
+import { useEffect } from "react";
+
+const Loading = () => {
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => (document.body.style.overflow = "auto");
+    }, []);
+
+    return (
+        <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-[9999]">
+            <div className="loader"></div>
+        </div>
+    );
+};
+export default Loading;
