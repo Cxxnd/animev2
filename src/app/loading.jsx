578fb81a6f65b217404ad81227b0479ce36fb9e5
Cxxnd <bwong0227@gmail.com>
2025-11-19 16:14:14 +0700

    add loading and notfound file

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center text-center">
+            <span class="loader">Load&nbsp;ng</span>
+        </div>
+    )
+}
+export default Loading
