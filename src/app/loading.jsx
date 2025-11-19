0386f7f5fd8ce7590bf421d519dbcba971b42e72
Cxxnd <bwong0227@gmail.com>
2025-11-20 01:56:12 +0700

    fix issue loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,20 @@
+"use client";
+import { useEffect } from "react";
 import "../components/css/loading.css";
+
 const Loading = () => {
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []);
+
     return (
         <div className="min-h-svh flex items-center justify-center">
             <span className="loader"></span>
         </div>
     );
 };
+
 export default Loading;
